2db7580535ea26ca2ddc84ca82d8b7c0a23cb198
Courtney Dahlson <courtneydahlson@gmail.com>
2025-04-29 16:16:27 -0400

    Modifyed API code

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-    API_GATEWAY_URL: "dummy url"
+    //API_GATEWAY_URL: "dummy url"
+    API_GATEWAY_URL: "https://l4xkgyumdd.execute-api.us-east-1.amazonaws.com/dev"
 };
--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -23,21 +23,21 @@ resource "aws_api_gateway_method" "post_ordersubmission" {
 }
 
 
-resource "aws_api_gateway_method_response" "post_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.apigateway.id
-  resource_id = aws_api_gateway_resource.ordersubmission.id
-  http_method = aws_api_gateway_method.post_ordersubmission.http_method
-  status_code = "200"
+# resource "aws_api_gateway_method_response" "post_method_response" {
+#   rest_api_id = aws_api_gateway_rest_api.apigateway.id
+#   resource_id = aws_api_gateway_resource.ordersubmission.id
+#   http_method = aws_api_gateway_method.post_ordersubmission.http_method
+#   status_code = "200"
 
-  response_models = {
-    "application/json" = "Empty"
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-  }
-}
+#   response_models = {
+#     "application/json" = "Empty"
+#   }
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Origin" = true
+#     "method.response.header.Access-Control-Allow-Methods" = true
+#     "method.response.header.Access-Control-Allow-Headers" = true
+#   }
+# }
 
 # resource "aws_api_gateway_integration_response" "post_integration_response" {
 #   rest_api_id = aws_api_gateway_rest_api.apigateway.id
