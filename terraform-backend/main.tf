a6d59e024d04d6e7159f259e18af12f1321a784c
Courtney Dahlson <courtneydahlson@gmail.com>
2025-04-29 17:11:57 -0400

    Added deployment

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -177,7 +177,11 @@ resource "aws_api_gateway_deployment" "deployment" {
       aws_api_gateway_rest_api.apigateway.id,
       aws_api_gateway_method.post_ordersubmission.id,
       aws_api_gateway_integration.lambda_integration.id,
-      aws_api_gateway_resource.ordersubmission.id
+      aws_api_gateway_resource.ordersubmission.id,
+      aws_api_gateway_method.options_method.id,
+      aws_api_gateway_integration.options_integration.id,
+      aws_api_gateway_method_response.options_method_response.id,
+      aws_api_gateway_integration_response.options_integration_response.id
     ]))
   }
 
