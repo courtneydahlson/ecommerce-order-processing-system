25cbcd10db65f1568aab07c278d07b003534b368
Courtney Dahlson <courtneydahlson@gmail.com>
2025-04-29 15:35:39 -0400

    Updated workflow

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -39,22 +39,22 @@ resource "aws_api_gateway_method_response" "post_method_response" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "get_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.apigateway.id
-  resource_id = aws_api_gateway_resource.ordersubmission.id
-  http_method = aws_api_gateway_method.post_ordersubmission.http_method
-  status_code = aws_api_gateway_method_response.post_method_response.status_code
+# resource "aws_api_gateway_integration_response" "post_integration_response" {
+#   rest_api_id = aws_api_gateway_rest_api.apigateway.id
+#   resource_id = aws_api_gateway_resource.ordersubmission.id
+#   http_method = aws_api_gateway_method.post_ordersubmission.http_method
+#   status_code = aws_api_gateway_method_response.post_method_response.status_code
 
-  response_templates = {
-    "application/json" = ""
-  }
+#   response_templates = {
+#     "application/json" = ""
+#   }
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET, POST, OPTIONS'"
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
-  }
-}
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Origin" = "'*'"
+#     "method.response.header.Access-Control-Allow-Methods" = "'GET, POST, OPTIONS'"
+#     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+#   }
+# }
 
 resource "aws_api_gateway_method" "options_method" {
   rest_api_id   = aws_api_gateway_rest_api.apigateway.id
